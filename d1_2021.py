b81bb1cd90ba5b9836447a3d82602443415b884d
clemkeirua <clement@keiruaprod.fr>
2021-12-01 09:56:21 +0100

    d1_2021

---
+++ d1_2021.py
@@ -0,0 +1,27 @@
+import aoc
+
+def parse(input:str):
+    lines = aoc.as_lines(input)
+    return aoc.as_ints(lines)
+
+def part_1(ints) -> int:
+    nb_inc = 0
+    for i in range(len(ints)-1):
+        if ints[i+1] > ints[i]:
+            nb_inc += 1
+    return nb_inc
+
+def part_2(ints) -> int:
+    nb_inc = 0
+    for i in range(len(ints)-3):
+        s = sum(ints[i:i+3])
+        s2 = sum(ints[i+1:i+4])
+        if s2 > s:
+            nb_inc += 1
+    return nb_inc
+
+if __name__ == "__main__":
+    input = aoc.input_as_string(aoc.challenge_filename(1, 2021))
+    ints = parse(input)
+    print(part_1((ints)))
+    print(part_2((ints)))
---
+++ test_d1_2021.py
@@ -0,0 +1,31 @@
+import d1_2021
+import unittest
+from hypothesis import given, strategies as st
+
+class TestFuzzDay1_2021(unittest.TestCase):
+    def test_part1_and_2(self):
+        test_input = """199
+        200
+        208
+        210
+        200
+        207
+        240
+        269
+        260
+        263"""
+        test_ints = d1_2021.parse(test_input)
+
+        self.assertEqual(d1_2021.part_1(test_ints), 7)
+        self.assertEqual(d1_2021.part_2(test_ints), 5)
+
+    @given(ints=st.lists(st.integers()))
+    def test_fuzz_part_1(self, ints):
+        d1_2021.part_1(ints=ints)
+
+    @given(ints=st.lists(st.integers()))
+    def test_fuzz_part_2(self, ints):
+        d1_2021.part_2(ints=ints)
+
+if __name__ == "__main__":
+    unittest.main()
