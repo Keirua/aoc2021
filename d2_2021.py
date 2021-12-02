dc12e7febc580e52af8fe5526d145fb3d6b3adca
clemkeirua <clement@keiruaprod.fr>
2021-12-02 08:45:43 +0100

    part 2

--- d2_2021.py
+++ d2_2021.py
@@ -1,26 +1,44 @@
 import aoc
-import re, pprint, itertools as it
-pp = pprint.PrettyPrinter(indent=4)
+import re
 
-input="""forward 5
+test_input="""forward 5
 down 5
 forward 8
 up 3
 down 8
 forward 2"""
 input = aoc.input_as_string(aoc.challenge_filename(2, 2021))
-matches = re.findall(r"(up|down|forward|) (\d+)", input)
-instructions = [(m, int(q)) for m, q in matches]
 
-depth, x = 0,0
-for (m, q) in instructions:
-    if m == "forward":
-        x += q
-    if m == "up":
-        depth -= q
-    if m == "down":
-        depth += q
+def parse(input):
+    matches = re.findall(r"(up|down|forward|) (\d+)", input)
+    return [(m, int(q)) for m, q in matches]
 
+def part1(instructions):
+    depth, x = 0,0
+    for (m, q) in instructions:
+        if m == "forward":
+            x += q
+        if m == "up":
+            depth -= q
+        if m == "down":
+            depth += q
+    return depth * x
 
-pp.pprint(instructions)
-print(depth*x)
+def part2(instructions):
+    depth, x, aim = 0,0,0
+    for (m, q) in instructions:
+        if m == "forward":
+            x += q
+            depth += aim * q
+        if m == "up":
+            aim -= q
+        if m == "down":
+            aim += q
+    return depth * x
+
+assert(part1(parse(test_input)) == 150)
+assert(part2(parse(test_input)) == 900)
+
+instructions = parse(input)
+print(part1(instructions))
+print(part2(instructions))
