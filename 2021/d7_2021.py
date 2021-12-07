2c37db30ff3b0eac9ff7c80caf656938a6eb9748
clemkeirua <clement@keiruaprod.fr>
2021-12-07 13:17:59 +0100

    day7 part2

--- 2021/d7_2021.py
+++ 2021/d7_2021.py
@@ -1,31 +1,46 @@
 import aoc
-import re, pprint, itertools as it
 from collections import Counter
-pp = pprint.PrettyPrinter(indent=4)
 
 def parse(input):
-    positions = aoc.all_ints(input)
-    return Counter(positions)
+    return Counter(aoc.all_ints(input))
 
-input = aoc.input_as_string(aoc.challenge_filename(7, 2021))
-test_input = "16,1,2,0,4,2,7,1,2,14"
-
-positions = parse(input)
-test_positions = parse(test_input)
 def score(positions, align_at):
-    s = 0
-    for k in positions.keys():
-        s += abs(align_at - k) * positions[k]
-    return s
+    return sum(abs(align_at - k) * positions[k] for k in positions.keys())
+
+def cost2(n): return n * (n + 1) // 2
+
+def score2(positions, align_at):
+    return sum(positions[k] * cost2(abs(align_at - k)) for k in positions.keys())
 
 def part1(positions):
     # all we have to do is minimize the score, and in order to do that the crabs have to go towards the center,
-    # so between the min and max value.
-    # so we compute the cost of movement for each position over this range
-    return min(score(positions, i) for i in range(min(positions.keys()), max(positions.keys())+1))
+    # so they can move between the min and max values in the list.
+    # We compute the cost of movement for each position over this range, and find the minimum
+    return min(score(positions, i) for i in range(min(positions.keys()), max(positions.keys()) + 1))
 
-assert(score(test_positions, 1) == 41)
-assert(score(test_positions, 3) == 39)
-assert(score(test_positions, 10) == 71)
-assert(part1(test_positions) == 37)
-print(part1(positions)) # max - min = 1904
+def part2(positions):
+    # same as part1, with a different scoring function
+    return min(score2(positions, i) for i in range(min(positions.keys()), max(positions.keys()) + 1))
+
+if __name__ == "__main__":
+    input = aoc.input_as_string(aoc.challenge_filename(7, 2021))
+    test_input = "16,1,2,0,4,2,7,1,2,14"
+
+    positions = parse(input)
+    test_positions = parse(test_input)
+
+    assert (score(test_positions, 1) == 41)
+    assert (score(test_positions, 3) == 39)
+    assert (score(test_positions, 10) == 71)
+    assert (part1(test_positions) == 37)
+    print(part1(positions))
+    assert (cost2(0) == 0)
+    assert (cost2(1) == 1)
+    assert (cost2(2) == 3)
+    assert (cost2(5) == 15)
+    assert (cost2(11) == 66)
+    assert (score2(test_positions, 5) == 168)
+    assert (score2(test_positions, 2) == 206)
+    assert (part2(test_positions) == 168)
+    assert (part2(positions) == 95167302)
+    print(part2(positions))
