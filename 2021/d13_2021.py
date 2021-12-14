f356e972a0415adfde6ef28d2419f57c95d1b568
clemkeirua <clement@keiruaprod.fr>
2021-12-14 16:46:34 +0100

    day 13 part 1 and 2

--- 2021/d13_2021.py
+++ 2021/d13_2021.py
@@ -97,6 +97,50 @@ def parse(input):
 
     return (g, folds)
 
-# grid, folds = parse(input)
-# grid, folds = parse(test_input)
-print(grid)
+def fold(grid:Grid, fold):
+    axis, value = fold
+    print(axis, value, fold)
+    if axis == 'y':
+        g2 = Grid.from_value(grid.w, value, '.')
+        for c in grid.all_coords():
+            # before the fold line, we copy the content
+            if c[1] < value and grid[c] != ".":
+                g2[c] = grid[c]
+            # after the fold line, we mirror the result
+            if c[1] > value  and grid[c] != ".":
+                mirror_pos = value - (c[1]-value)
+                if mirror_pos >=0:
+                    g2[(c[0], mirror_pos)] = grid[c]
+        return g2
+    if axis == 'x':
+        g2 = Grid.from_value(value, grid.h, '.')
+        for c in grid.all_coords():
+            # before the fold line, we copy the content
+            if c[0] < value and grid[c] != ".":
+                g2[c] = grid[c]
+            # after the fold line, we mirror the result
+            if c[0] > value and grid[c] != ".":
+                mirror_pos = value - (c[0] - value)
+                if mirror_pos >= 0:
+                    g2[(mirror_pos, c[1])] = grid[c]
+        return g2
+
+def part2(grid, folds):
+    for f in folds:
+        grid = fold(grid, f)
+    print(grid)
+
+def part1(grid:Grid, folds):
+    grid = fold(grid, folds[0])
+    nb_lit = 0
+    for c in grid.all_coords():
+        if grid[c] == "#":
+            nb_lit += 1
+    return nb_lit
+
+grid, folds = parse(input)
+test_grid, test_folds = parse(test_input)
+
+assert(part1(test_grid, test_folds) ==17)
+print(part1(grid, folds))
+part2(grid, folds) # CPJBERUL
