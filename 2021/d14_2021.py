d420979186c3876cae9bcac1b985af08d552501e
clemkeirua <clement@keiruaprod.fr>
2021-12-14 22:22:44 +0100

    more clever implementation for part2

--- 2021/d14_2021.py
+++ 2021/d14_2021.py
@@ -26,19 +26,15 @@ CN -> C"""
 
 
 def parse(input):
-    lines = aoc.as_lines(input)
     new_pairs = {}
     replacement = {}
     new_letter = {}
-    for line in lines[2:]:
-        k, v = line.strip().split(" -> ")
-        k = k.strip()
-        v = v.strip()
+    for k, v in re.findall(r"(.*) -> (.*)", input):
         new_pairs[k] = [f"{k[0]}{v}", f"{v}{k[1]}"]
         new_letter[k] = v
         replacement[k] = f"{v}{k[1]}"
 
-    return lines[0].strip(), new_pairs, replacement, new_letter
+    return input[:input.find("\n")], new_pairs, replacement, new_letter
 
 
 def step(template, rules):
@@ -50,52 +46,29 @@ def step(template, rules):
     return res
 
 
-def score(template):
+def part1(template, rules, n=5):
+    """naive implementation, doesnt scale fast past n = 15 due to growth that follows fibonacci"""
+    for i in range(1, n + 1):
+        template = step(template, rules)
     c = Counter(list(template))
     return max(c.values()) - min(c.values())
 
 
-def part1(template, rules, n=5):
-    for i in range(1, n + 1):
-        template = step(template, rules)
-    return score(template)
-
-
-def step2(template, mapping):
-    pairs = [template[i:i + 2] for i in range(len(template) - 1)]
-    new_pairs = [mapping[p] for p in pairs]
-    print("NCNBCHB")
-    print(pairs)
-    print(new_pairs)
-
-
-from collections import defaultdict
-
-
 def part2(template, rules, new_letters, n=5):
-    pairs = [template[i:i + 2] for i in range(len(template) - 1)]
+    letters_count = Counter(list(template))
+    pair_count = Counter([template[i:i + 2] for i in range(len(template) - 1)])
 
-    letters_count = defaultdict(int)
-    pair_count = defaultdict(int)
-    for c in template:
-        letters_count[c] += 1
-    for p in pairs:
-        pair_count[p] += 1
     for i in range(1, n + 1):
-        new_letter_count = letters_count.copy()
-        # new_pair_count = pair_count.copy()
-        new_pair_count = defaultdict(int)
+        new_pair_count = Counter()
         for p in pair_count.keys():
             # So each pair creates 2 pair and creates one new letter
             pair_a, pair_b = rules[p]
             new_letter = new_letters[p]
             # all we have to do is keep track of the letter counts and the pair counts
-            new_letter_count[new_letter] += pair_count[p]
+            letters_count[new_letter] += pair_count[p]
             new_pair_count[pair_a] += pair_count[p]
             new_pair_count[pair_b] += pair_count[p]
         pair_count = new_pair_count
-        letters_count = new_letter_count
-        # pp.pprint(pair_count)
 
     return max(letters_count.values()) - min(letters_count.values())
 
