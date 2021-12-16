6fa4b1cd0507afe1965e51629e222c42fcf9d1fd
clemkeirua <clement@keiruaprod.fr>
2021-12-16 22:11:31 +0100

    seemingly correct parsing but in fact no

--- 2021/d16_2021.py
+++ 2021/d16_2021.py
@@ -50,40 +50,55 @@ class Packet:
     length: int
     sub_packets: list
 
+sum_version = 0
 
 def extract(bits):
-    # 110100101111111000101000
-    # VVVTTTAAAAABBBBBCCCCC
-    # bits = "110100101111111000101000"
+    global sum_version
     v = to_int(bits[0:3])
     t = to_int(bits[3:6])
+    print(f"v: {v}")
+    print(f"t: {t}")
     if t == 4:
+        print("literal value packet")
         data = ""
         i = 0
         # parse type4: literal data
         # read by chunk of 5 bits, stop when the first bit of the last chunk is 0
-        done = False
-        while not done:
+        while True:
             # the actual information is stored in the remaining 4 bits
             data += bits[6+5*i+1:6+5*i+5]
             # the first bit indicates when to stop
             if bits[6+5*i] == '0':
                 break
             i += 1
+
+        sum_version += v
         return Packet(v, t, to_int(data), None, []), 6+5*i+5
     else:
+        print("operator packet")
         # everything that is not 4 is an operator packet
         # An operator packet contains one or more packets
         length_type_id = int(bits[6])
+        print(f"length_type_id: {length_type_id}")
         if length_type_id == 0:
             # If the length type ID is 0, then the next 15 bits are a number
             # that represents the total length in bits of the sub-packets contained by this packet.
             length = to_int(bits[7:7+15])
-            raise ValueError("not implemented")
+            print(f"length: {length}")
+            total_offset = 0
+            packets = []
+            while total_offset < length:
+                new_packet, offset = extract(bits[22 + total_offset:])
+                total_offset += offset
+                packets.append(new_packet)
+            sum_version += v
+            return Packet(v, t, None, total_offset, packets), length
         else:
+            assert(length_type_id == 1), length_type_id
             # If the length type ID is 1, then the next 11 bits are a number
             # that represents the number of sub-packets immediately contained by this packet.
             number_of_sub_packets = to_int(bits[7:7 + 11])
+            print(f"number_of_sub_packets: {number_of_sub_packets}")
             packets = []
             total_offset = 0
             for i in range(number_of_sub_packets):
@@ -91,27 +106,22 @@ def extract(bits):
                 # print(new_packet, offset)
                 total_offset += offset
                 packets.append(new_packet)
+            sum_version += v
             return Packet(v,t, None, total_offset, packets), total_offset
 
+def part1(hex_packet):
+    global sum_version
+    sum_version = 0
+    bits = parse(hex_packet)
+    packets, length = extract(bits)
+    return sum_version
 
-    # raise ValueError(t)
+# assert(extract("110100101111111000101000") == (Packet(version=6, type=4, data=2021, length=None, sub_packets=[]), 21))
+# assert(extract("11101110000000001101010000001100100000100011000001100000") == (Packet(version=7, type=3, data=None, length=33, sub_packets=[Packet(version=2, type=4, data=1, length=None, sub_packets=[]), Packet(version=4, type=4, data=2, length=None, sub_packets=[]), Packet(version=1, type=4, data=3, length=None, sub_packets=[])]),33))
+# assert(extract("00111000000000000110111101000101001010010001001000000000") == (   Packet(version=1, type=6, data=None, length=27, sub_packets=[Packet(version=6, type=4, data=10, length=None, sub_packets=[]), Packet(version=2, type=4, data=20, length=None, sub_packets=[])]),27))
 
-# type 4 operator:
-# So, this packet represents a literal value with binary representation
-# 011111100101, which is 2021 in decimal.
-# 110100101111111000101000
-# VVVTTTAAAAABBBBBCCCCC
+# assert(part1("8A004A801A8002F478") == 16)
+assert(part1("620080001611562C8802118E34") == 12)
+# assert(part1("C0015000016115A2E0802F182340") == 23)
+# assert(part1("A0016C880162017C3686B18A3D4780") == 31)
 
-# length type 0:
-# 00111000000000000110111101000101001010010001001000000000
-# VVVTTTILLLLLLLLLLLLLLLAAAAAAAAAAABBBBBBBBBBBBBBBB
-
-# length type 1
-# 11101110000000001101010000001100100000100011000001100000
-# VVVTTTILLLLLLLLLLLAAAAAAAAAAABBBBBBBBBBBCCCCCCCCCCC
-
-# pp.pprint(input)
-# bits = parse(input)
-# pp.pprint(extract(bits))
-assert(extract("110100101111111000101000") == (Packet(version=6, type=4, data=2021, length=None, sub_packets=[]), 21))
-assert(extract("11101110000000001101010000001100100000100011000001100000") == (Packet(version=7, type=3, data=None, length=33, sub_packets=[Packet(version=2, type=4, data=1, length=None, sub_packets=[]), Packet(version=4, type=4, data=2, length=None, sub_packets=[]), Packet(version=1, type=4, data=3, length=None, sub_packets=[])]),33))
