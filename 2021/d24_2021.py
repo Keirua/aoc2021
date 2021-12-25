2d2f36470c132d38d0b7cbb7568a82b5f6dd1341
clemkeirua <clement@keiruaprod.fr>
2021-12-25 22:16:46 +0100

    so visibly, all input with between 1000000 and 100000 lead to z=11817

--- 2021/d24_2021.py
+++ 2021/d24_2021.py
@@ -17,18 +17,21 @@ class Alu:
     def __init__(self, instructions):
         self.instructions = instructions
 
-    def run_with(self, input_data):
+    def run_with(self, input_data, max_nb_inp=15):
         self.vars = { k: 0 for k in valid_vars}
         self.input_data = input_data
         self.input_pos = 0
-        return self.run_program()
+        self.nb_inp = 0
+        return self.run_program(max_nb_inp)
 
-    def run_program(self):
+    def run_program(self, max_nb_inp):
         # print(self.input_data)
         # Then, after MONAD has finished running all of its instructions, it will indicate that the model number was
         # valid by leaving a 0 in variable z. However, if the model number was invalid, it will leave some other
         # non-zero value in z.
         for i in self.instructions:
+            if self.nb_inp > max_nb_inp:
+                break
             self.run_instruction(i)
             # print("valueError Reached")
         return self.vars["z"] == 1
@@ -43,9 +46,11 @@ class Alu:
         # print(instr)
         if instr[0] == "inp":
             assert(instr[1] in valid_vars)
+            assert(self.input_pos < len(self.input_data)), len(self.input_data)
             pp.pprint(self.vars)
             self.vars[instr[1]] = int(self.input_data[self.input_pos])
             self.input_pos += 1
+            self.nb_inp += 1
             return
         a = self.vars[instr[1]]
         b = self.get_val(instr[2])
@@ -95,6 +100,17 @@ def parse(input):
 # x = 1
 # y = (w + 11)
 # z = z*26+(w + 11)
+#
+# third block as python code
+# inp w
+# x = (z%26)+13
+# if x != w:
+# 	x = 1
+# 	y = (w + 12)
+# 	z = z*26+(w + 12)
+# else:
+# 	x = 0
+# 	y = 0
 
 input = aoc.input_as_string(aoc.challenge_filename(24, 2021))
 a = Alu(parse(input))
@@ -102,13 +118,13 @@ a = Alu(parse(input))
 # print(a.run_with("13111111111111"))
 first_inp = aoc.input_as_string("input/first_part_24.txt")
 first_inp_instructions = parse(first_inp)
-a = Alu(first_inp_instructions)
-for i in range(9, 0, -1):
-    v = str(i)
-    print("--------------")
-    print(v)
-    print (a.run_with(v))
-    pp.pprint(a.vars)
+# a = Alu(first_inp_instructions)
+# for i in range(9, 0, -1):
+#     v = str(i)
+#     print("--------------")
+#     print(v)
+#     print (a.run_with(v))
+#     pp.pprint(a.vars)
 # second_inp = aoc.input_as_string("input/second_part_24.txt")
 # second_instr = parse(second_inp)
 # a = Alu(second_instr)
@@ -120,3 +136,18 @@ for i in range(9, 0, -1):
 #     print(v)
 #     print (a.run_with(v))
 #     pp.pprint(a.vars)
+# third_instr = parse(aoc.input_as_string("input/third_part_24.txt"))
+# a = Alu(third_instr)
+small_values = []
+for i in range(1000000, 100000, -1):
+    v = str(i)
+    if "0" in v:
+        continue
+    print("--------------")
+    print(v)
+    print(a.run_with(v, 3))
+    # print(a.vars["z"])
+    print(a.vars["z"])
+    if a.vars["z"] < 11800:
+        small_values.append(v)
+pp.pprint(small_values)
