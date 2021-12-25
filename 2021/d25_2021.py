a8f6f4a3792c08b9784d55c7b0642b8dd073074d
clemkeirua <clement@keiruaprod.fr>
2021-12-25 21:08:24 +0100

    day 25, an easy one at last

--- 2021/d25_2021.py
+++ 2021/d25_2021.py
@@ -1,6 +1,7 @@
 import aoc
 import re, pprint, itertools as it
 from dataclasses import dataclass
+
 pp = pprint.PrettyPrinter(indent=4)
 
 input = aoc.input_as_string(aoc.challenge_filename(25, 2021))
@@ -14,6 +15,7 @@ v>v.vv.v..
 v.v..>>v.v
 ....v..v.>"""
 
+
 @dataclass
 class Grid:
     map: list
@@ -26,23 +28,51 @@ class Grid:
             o += "".join(self.map[y]) + "\n"
         return o
 
-def parse(input):
-    lines = aoc.as_lines(input)
-    grid = [list(l.rstrip()) for l in lines]
-    w, h = len(grid[0]), len(grid)
-    return Grid(grid, w, h)
 
-def step(g:Grid):
-    grid2 = [["." for _ in range(g.w)] for _ in range(g.h)]
-    # first, the east-facing sea cucumbers try to move east
+def parse(input: str) -> Grid:
+    lines = aoc.as_lines(input)
+    map = [list(l.rstrip()) for l in lines]
+    return Grid(map, len(map[0]), len(map))
+
+
+def step(g: Grid):
+    moved = False
+    # We create an empty map
+    map2 = [["." for _ in range(g.w)] for _ in range(g.h)]
+    # first, all the east-facing sea cucumbers try to move east
     for y in range(g.h):
         for x in range(g.w):
-            if x+1 < g.w:
-                pass
-    return Grid(grid2, g.w, g.h)
+            nx = (x + 1) % g.w
+            if g.map[y][x] == ">":
+                if g.map[y][nx] == ".":
+                    map2[y][nx] = ">"
+                    moved = True
+                else:
+                    map2[y][x] = ">"
+    # Then, the south-facing herd attempt to move
+    for x in range(g.w):
+        for y in range(g.h):
+            if g.map[y][x] == "v":
+                ny = (y + 1) % g.h
+                if map2[ny][x] == "." and g.map[ny][x] != "v":
+                    map2[ny][x] = "v"
+                    moved = True
+                else:
+                    map2[y][x] = "v"
+
+    return Grid(map2, g.w, g.h), moved
+
+
+def part1(grid):
+    nb_steps = 0
+    while True:
+        grid, moved = step(grid)
+        nb_steps += 1
+        if not moved:
+            return nb_steps
+
 
 grid = parse(input)
 test_grid = parse(test_input)
-grid2 = step(test_grid)
-print(test_grid)
-print(grid2)
+assert (part1(test_grid) == 58)
+print(part1(grid))
