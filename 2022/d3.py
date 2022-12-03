6babc7652fe028f0570a19efcd722a6671182797
clemkeirua <clement@keiruaprod.fr>
2022-12-03 14:05:03 +0100

    d3 p2

--- 2022/d3.py
+++ 2022/d3.py
@@ -5,17 +5,32 @@ wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw"""
 
-# data = sample.split("\n")
-data = [l.strip() for l in open("d3.txt").readlines()]
-total = 0
-for d in data:
-    s = len(d)
-    l, r = set(d[0:s//2]), set(d[s//2:])
-    common = ord(list(l&r)[0])
+def score(common):
     if ord('a') <= common <= ord('z'):
-        total += common - ord('a') +1
+        return common - ord('a') + 1
     else:
-        total += common - ord('A') +27
+        return common - ord('A') + 27
 
-print(total)
-# print(data)
+def part1(data):
+    total = 0
+    for d in data:
+        s = len(d)
+        l, r = set(d[0:s//2]), set(d[s//2:])
+        common = ord(list(l&r)[0])
+        total += score(common)
+    return total
+
+def part2(data):
+    total = 0
+    for l in range(0, len(data), 3):
+        a, b, c = set(data[l]), set(data[l+1]), set(data[l+2])
+        common = ord(list(a&b&c)[0])
+        s = score(common)
+        total += s
+    return total
+
+data = [l.strip() for l in open("d3.txt").readlines()]
+# data = sample.split("\n")
+p1 = part1(data)
+p2 = part2(data)
+print(p1, p2)
