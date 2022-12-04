94aaa5e9a2da7b11799ed59ab531a5538ac8ab78
clemkeirua <clement@keiruaprod.fr>
2022-12-04 14:15:11 +0100

    golfed the solution

--- 2022/d4.py
+++ 2022/d4.py
@@ -1,24 +1,26 @@
 import re, pprint, itertools as it
+
 pp = pprint.PrettyPrinter(indent=4)
 
 input = open("d4-sample.txt")
 input = open("d4.txt")
 
-def is_in(a,b,c,d):
-    return c in range(a, b + 1) and d in range(a, b + 1)
+
+def is_in(a, b, c, d): return c in range(a, b + 1) and d in range(a, b + 1)
+
 
 p1 = 0
 p2 = 0
+# for line in input:
+# l,r = line.split(",")
+# a, b = l.split("-")
+# c, d = r.split("-")
+# a,b,c,d = int(a), int(b), int(c), int(d)
 for line in input:
-    l,r = line.split(",")
-    a, b = l.split("-")
-    c, d = r.split("-")
-    a,b,c,d = int(a), int(b), int(c), int(d)
-    if is_in(a,b,c,d) or is_in(c,d, a, b):
-        p1 += 1
-    r1 = set(list(range(a, b+1)))
-    r2 = set(list(range(c, d+1)))
-    if len(r1&r2) > 0:
-        p2 += 1
+    a, b, c, d = [int(v) for v in re.findall(r'(\d+)', line)]
+    p1 += is_in(a, b, c, d) or is_in(c, d, a, b)
+    r1 = set(range(a, b + 1))
+    r2 = set(range(c, d + 1))
+    p2 += len(r1 & r2) > 0
 pp.pprint(p1)
 pp.pprint(p2)
