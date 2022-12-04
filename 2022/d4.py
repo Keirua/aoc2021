337ac2ddb428e84be3c91d9f0a8d66392d1725e9
clemkeirua <clement@keiruaprod.fr>
2022-12-04 13:49:24 +0100

    p1

--- 2022/d4.py
+++ 2022/d4.py
@@ -1,9 +1,18 @@
 import re, pprint, itertools as it
 pp = pprint.PrettyPrinter(indent=4)
 
-#input = open("d4-sample.txt")
+input = open("d4-sample.txt")
 input = open("d4.txt")
 
-lines = [l.strip() for l in input]
+def is_in(a,b,c,d):
+    return c in range(a, b + 1) and d in range(a, b + 1)
 
-pp.pprint(lines)
+p1 = 0
+for line in input:
+    l,r = line.split(",")
+    a, b = l.split("-")
+    c, d = r.split("-")
+    a,b,c,d = int(a), int(b), int(c), int(d)
+    if is_in(a,b,c,d) or is_in(c,d, a, b):
+        p1 += 1
+pp.pprint(p1)
