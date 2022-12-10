a005f1495a2acfcc3c1e0d6ddffd2469b5005ba2
clemkeirua <clement@keiruaprod.fr>
2022-12-10 14:21:41 +0100

    d8 p2 + d9 wip

--- 2022/d8.py
+++ 2022/d8.py
@@ -2,8 +2,7 @@ import aoc
 import re, pprint, itertools as it
 pp = pprint.PrettyPrinter(indent=4)
 from grid import *
-input = open(f"d8.txt").read()
-lines = input.split("\n")
+
 
 class TreeGrid(Grid):
     @classmethod
@@ -20,21 +19,52 @@ class TreeGrid(Grid):
             cy += dy
         return values
 
-t = TreeGrid.from_lines(lines)
 
 def rmax(v):
     if len(v) == 0:
         return -1
     return max(v)
 
-nb  = 0
-for (x, y) in t.all_coords():
-    left = rmax(t.all_neighbours_in_dir(x, y, -1, 0))
-    right = rmax(t.all_neighbours_in_dir(x, y, 1, 0))
-    top = rmax(t.all_neighbours_in_dir(x, y, 0, 1))
-    down = rmax(t.all_neighbours_in_dir(x, y, 0, -1))
-    v = t.get(x, y)
-    nb += any([v > u for u in [left, right, top, down]])
+def part1(t):
+    nb = 0
+    for (x, y) in t.all_coords():
+        left = rmax(t.all_neighbours_in_dir(x, y, -1, 0))
+        right = rmax(t.all_neighbours_in_dir(x, y, 1, 0))
+        top = rmax(t.all_neighbours_in_dir(x, y, 0, 1))
+        down = rmax(t.all_neighbours_in_dir(x, y, 0, -1))
+        v = t.get(x, y)
+        nb += any([v > u for u in [left, right, top, down]])
+    return nb
 
-print(nb)
+def nb_vis(neighbours, v):
+    nb = 1
+    for n in neighbours:
+        if n >= v:
+            return nb
+        else:
+            nb += 1
+    return len(neighbours)
+import math
+def part2(t):
+    nb = 0
+    # x, y = 2, 3
+    for (x, y) in t.all_coords():
+        left = t.all_neighbours_in_dir(x, y, -1, 0)
+        right = t.all_neighbours_in_dir(x, y, 1, 0)
+        top = t.all_neighbours_in_dir(x, y, 0, 1)
+        down = t.all_neighbours_in_dir(x, y, 0, -1)
+        v = t.get(x, y)
+        prods = [nb_vis(u, v) for u in [left, right, top, down]]
+    # print(top)
+    # print(prods)
+
+        curr = math.prod(prods)
+        nb = max(curr, nb)
+    return nb
+
+input = open(f"d8.txt").read()
+lines = input.split("\n")
+t = TreeGrid.from_lines(lines)
+# print(part1(t))
+print(part2(t))
 
