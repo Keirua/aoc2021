c71606334059d3663ff28d4c8de8f3743fe674b5
clemkeirua <clement@keiruaprod.fr>
2022-12-11 15:17:03 +0100

    with a Counter

--- 2022/d11.py
+++ 2022/d11.py
@@ -4,7 +4,7 @@ pp = pprint.PrettyPrinter(indent=4)
 input = open(f"d11.txt").read()
 input = open(f"d11-sample.txt").read()
 lines = input.split("\n")
-from collections import deque
+from collections import deque, Counter
 class Monkey:
     def __init__(self):
         self.items = deque()
@@ -22,30 +22,41 @@ class Monkey:
 monkeys = []
 for i in range(0, len(lines), 7):
     m = Monkey()
+    items = []
     for s in re.findall("(\d+)", lines[i + 1]):
-        m.items.append(int(s))
+        items.append(int(s))
+    m.items = Counter(items)
     m.op = re.findall("new = (.*)", lines[i+2])[0]
     m.test = int(re.findall("(\d+)", lines[i+3])[0])
     m.on_true = int(re.findall("(\d+)", lines[i+4])[0])
     m.on_false = int(re.findall("(\d+)", lines[i+5])[0])
     monkeys.append((m))
 
+# for j in range(20):
+#     for i,m in enumerate(monkeys):
+#         for i in range(len(m.items)):
+#             old = m.items.popleft()
+#             n = eval(m.op) // 3
+#             m.nb_inspect+=1
+#             if n % m.test == 0:
+#                 monkeys[m.on_true].items.append(n)
+#             else:
+#                 monkeys[m.on_false].items.append(n)
 
-def compute_runs(monkeys, N = 20):
-    for nb_runs in range(N):
-        for m in monkeys:
-            nb_items = len(m.items)
-            new_items = []
-            for old in m.items:
-                new_items.append(eval(m.op))
-            to_true = [v for v in new_items if v % m.test == 0]
-            to_false = [v for v in new_items if v % m.test != 0]
-            monkeys[m.on_true].items += to_true
-            monkeys[m.on_false].items += to_false
-            m.items = deque()
-            m.nb_inspect += nb_items
-    inspects = list(sorted([m.nb_inspect for m in monkeys]))
-    pp.pprint(inspects)
-    return(inspects[-1]*inspects[-2])
+for nb_runs in range(1000):
+    if nb_runs % 100 == 0:
+        print(nb_runs)
+    for m in monkeys:
+        for old, nb_old in m.items.items():
+            v = eval(m.op) // 3
+            if v % m.test == 0:
+                monkeys[m.on_true].items[v] += nb_old
+            else:
+                monkeys[m.on_false].items[v] += nb_old
+            m.nb_inspect += nb_old
+        m.items = Counter()
 
-print(compute_runs(monkeys, 1000))
+inspects = list(sorted([m.nb_inspect for m in monkeys]))
+pp.pprint(inspects)
+print(inspects[-1]*inspects[-2])
+
