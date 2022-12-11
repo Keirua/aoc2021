6dbb75e4c76d466cc5b876acc4776859cc4d5d1b
clemkeirua <clement@keiruaprod.fr>
2022-12-11 14:49:10 +0100

    part

--- 2022/d11.py
+++ 2022/d11.py
@@ -2,6 +2,7 @@ import re, pprint, itertools as it
 pp = pprint.PrettyPrinter(indent=4)
 
 input = open(f"d11.txt").read()
+# input = open(f"d11-sample.txt").read()
 lines = input.split("\n")
 from collections import deque
 class Monkey:
@@ -11,49 +12,36 @@ class Monkey:
         self.test = None
         self.on_true = None
         self.on_false = None
+        self.nb_inspect = 0
 
     def __repr__(self):
-        items = "".join(list(map(str, self.items)))
+        items = ", ".join(list(map(str, self.items)))
         return f"<Monkey items={items} op={self.op} test={self.test} true={self.on_true} false={self.on_false}>"
 
-    def calc_update(self, v, op):
-        # todo : nice hack to test with eval/exec
-        if op == 'old * 7':
-            return v*7
-
-        if op == 'old * old':
-            return v*v
-
-        if op == 'old + 8':
-            return v+8
-
-        if op == 'old + 4':
-            return v+4
-
-        if op == 'old + 3':
-            return v+3
-
-        if op == 'old + 5':
-            return v+5
-
-        if op == 'old + 7':
-            return v+7
-
-        if op == 'old * 3':
-            return v*7
 
 monkeys = []
 
 for i in range(0, len(lines), 7):
     m = Monkey()
-    m.items.append([int(s) for s in re.findall("(\d+)", lines[i+1])])
+    for s in re.findall("(\d+)", lines[i + 1]):
+        m.items.append(int(s))
     m.op = re.findall("new = (.*)", lines[i+2])[0]
     m.test = int(re.findall("(\d+)", lines[i+3])[0])
     m.on_true = int(re.findall("(\d+)", lines[i+4])[0])
     m.on_false = int(re.findall("(\d+)", lines[i+5])[0])
     monkeys.append((m))
 
-pp.pprint(monkeys)
-# for i,m in enumerate(monkeys):
-#     for i in range(len(m.items)):
-#         m.items.pop()
+for j in range(20):
+    for i,m in enumerate(monkeys):
+        for i in range(len(m.items)):
+            old = m.items.popleft()
+            n = eval(m.op) // 3
+            m.nb_inspect+=1
+            if n % m.test == 0:
+                monkeys[m.on_true].items.append(n)
+            else:
+                monkeys[m.on_false].items.append(n)
+print()
+# pp.pprint(monkeys)
+inspects = list(sorted([m.nb_inspect for m in monkeys]))
+print(inspects[-1]*inspects[-2])
