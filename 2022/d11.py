94eb942677633d145e669ea062fae19eec5884fe
clemkeirua <clement@keiruaprod.fr>
2022-12-11 22:03:27 +0100

    better constructor

--- 2022/d11.py
+++ 2022/d11.py
@@ -1,34 +1,39 @@
 import re, pprint as pp
 from math import prod
 import copy
-from typing import List
+from typing import List, Callable, Optional
 from collections import Counter
+from dataclasses import dataclass
+
+
+@dataclass
 class Monkey:
-    def __init__(self):
-        self.items = None
-        self.operation_code = ""
-        self.op = None
-        self.test = None
-        self.on_true = None
-        self.on_false = None
-        self.nb_inspect = 0
+    items: Counter
+    operation_code: str = ""
+    op: Callable[[int], int] = None
+    test: Optional[int] = None
+    on_true: Optional[int] = None
+    on_false: Optional[int] = None
+    nb_inspect: int = 0
 
     @classmethod
-    def from_lines(cls, lines):
-        m = cls()
-        m.items = Counter([int(s) for s in re.findall("(\d+)", lines[1])])
-        m.operation_code = re.search(r"new = (.*)", lines[2]).group(1)
-        m.op = lambda old: eval(m.operation_code)
-        m.test = int(lines[3].split()[-1])
-        m.on_true = int(lines[4].split()[-1])
-        m.on_false = int(lines[5].split()[-1])
-        return m
+    def from_lines(cls: "Monkey", lines) -> "Monkey":
+        operation_code = re.search(r"new = (.*)", lines[2]).group(1)
+        return cls(
+            items=Counter([int(s) for s in re.findall("(\d+)", lines[1])]),
+            operation_code=operation_code,
+            op=lambda old: eval(operation_code),
+            test=int(lines[3].split()[-1]),
+            on_true=int(lines[4].split()[-1]),
+            on_false=int(lines[5].split()[-1])
+        )
 
     def __repr__(self):
         items = ", ".join(list(map(str, self.items)))
         return f"<Monkey items={items} op={self.operation_code} test={self.test} true={self.on_true} false={self.on_false}>"
 
-def parse(raw_monkeys:List[str]) -> List[Monkey]:
+
+def parse(raw_monkeys: List[str]) -> List[Monkey]:
     return [Monkey.from_lines(m.split("\n")) for m in raw_monkeys]
 
 
@@ -49,14 +54,14 @@ def run(monkeys: List[Monkey], part: int, N: int) -> int:
             m.items = Counter()
 
     inspects = sorted([m.nb_inspect for m in monkeys])
-    return inspects[-1]*inspects[-2]
+    return inspects[-1] * inspects[-2]
 
 
 test_input = open(f"d11-sample.txt").read()
 test_monkeys = parse(test_input.split("\n\n"))
 test_monkeys2 = copy.deepcopy(test_monkeys)
-assert(run(test_monkeys, 1, 20) == 10605)
-assert(run(test_monkeys2, 2, 10000) == 2713310158)
+assert (run(test_monkeys, 1, 20) == 10605)
+assert (run(test_monkeys2, 2, 10000) == 2713310158)
 
 input = open(f"d11.txt").read()
 lines = input.split("\n\n")
