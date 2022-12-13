63b52135a5e5df451d5c8bf0a6fe9b8324f487d2
clemkeirua <clement@keiruaprod.fr>
2022-12-13 21:23:59 +0100

    day 9 part2

--- 2022/d9.py
+++ 2022/d9.py
@@ -1,48 +1,55 @@
-import re, pprint, itertools as it
-
-pp = pprint.PrettyPrinter(indent=4)
-
-input = open(f"d9.txt").read()
-# input = open(f"d9-sample.txt").read()
-moves = [(dir, int(dist)) for (dir, dist) in re.findall(r"([RDLU]) (\d+)", input)]
-mapping = {
-    "R": (1, 0),
-    "L": (-1, 0),
-    "U": (0, 1),
-    "D": (0, -1),
-}
-head_positions = [(0, 0)]
-tail_positions = [(0, 0)]
-
-for dir, dist in moves:
-    dx, dy = mapping[dir]
-    for i in range(dist):
-        xh, yh = head_positions[-1]
-        xt, yt = tail_positions[-1]
-        # Compute the new head position
-        xh, yh = xh+ dx, yh + dy
-        # Compute the movement between new head and previous tail
-        dthx, dthy = xh - xt, yh - yt
-        # If we moved enough, we need to update the tail to the previous head position
-        if abs(dthx) > 1 or abs(dthy) > 1:
-            tail_positions.append(head_positions[-1])
-        head_positions.append((xh, yh))
-
-def plot(positions):
-    W = max([x for (x, y) in positions]) + 1
-    H = max([y for (x, y) in positions]) + 1
-    arr = [["." for w in range(W)] for h in range(H)]
-    for index, (x, y) in enumerate(positions):
-        arr[y][x] = str(index)
-    max_cell_size = max([len(arr[y][x]) for x in range(W) for y in range(H)])
-    s = ""
-    for y in range(H):
-        s += "".join([c.center(1 + max_cell_size) for c in arr[H - y - 1]]) + "\n"
-    return s
+import re
+from typing import List, Tuple
 
 
-# print(plot(head_positions))
-# print()
-# print(plot(tail_positions))
-print(len(set(tail_positions)))
-# print(len(set(head_positions[:-1])))
+class Tail:
+    def __init__(self):
+        self.x: int = 0
+        self.y: int = 0
+        self.history: List[Tuple[int, int]] = []
+
+    def follow(self, pos: Tuple[int, int]):
+        x, y = pos
+        dist_x = x - self.x
+        dist_y = y - self.y
+        dx = 1 if dist_x > 0 else -1
+        dy = 1 if dist_y > 0 else -1
+        if abs(dist_x) == 2 and dist_y == 0:
+            self.x += dx
+        elif abs(dist_y) == 2 and dist_x == 0:
+            self.y += dy
+        elif (abs(dist_y) == 2 and abs(dist_x) in (1, 2)) or (abs(dist_x) == 2 and abs(dist_y) in (1, 2)):
+            self.x += dx
+            self.y += dy
+        self.history.append((self.x, self.y))
+
+
+def part(moves: List[Tuple[str, int]], tails: List[Tail]) -> int:
+    head: List[Tuple[int, int]] = [(0, 0)]
+    mapping = {
+        "R": (1, 0),
+        "L": (-1, 0),
+        "U": (0, 1),
+        "D": (0, -1),
+    }
+    for dir, dist in moves:
+        dx, dy = mapping[dir]
+        for i in range(dist):
+            xh, yh = head[-1]
+            xh, yh = xh + dx, yh + dy
+            head.append((xh, yh))
+            prev = head[-1]
+            for r in range(len(tails)):
+                tails[r].follow(prev)
+                prev = tails[r].history[-1]
+
+    return len(set(tails[-1].history))
+
+
+if __name__ == "__main__":
+    input = open(f"d9.txt").read()
+    # input = open(f"d9-sample.txt").read()
+    moves = [(dir, int(dist)) for (dir, dist) in re.findall(r"([RDLU]) (\d+)", input)]
+
+    print(part(moves, [Tail()]))
+    print(part(moves, [Tail() for _ in range(9)]))
