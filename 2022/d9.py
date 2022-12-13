aa08a32a13d4f94d837c00779a25f27e94792668
clemkeirua <clement@keiruaprod.fr>
2022-12-13 21:01:17 +0100

    day9 part1

--- 2022/d9.py
+++ 2022/d9.py
@@ -3,7 +3,7 @@ import re, pprint, itertools as it
 pp = pprint.PrettyPrinter(indent=4)
 
 input = open(f"d9.txt").read()
-input = open(f"d9-sample.txt").read()
+# input = open(f"d9-sample.txt").read()
 moves = [(dir, int(dist)) for (dir, dist) in re.findall(r"([RDLU]) (\d+)", input)]
 mapping = {
     "R": (1, 0),
@@ -12,14 +12,21 @@ mapping = {
     "D": (0, -1),
 }
 head_positions = [(0, 0)]
-x, y = head_positions[-1]
+tail_positions = [(0, 0)]
+
 for dir, dist in moves:
     dx, dy = mapping[dir]
     for i in range(dist):
-        x += dx
-        y += dy
-        head_positions.append((x, y))
-
+        xh, yh = head_positions[-1]
+        xt, yt = tail_positions[-1]
+        # Compute the new head position
+        xh, yh = xh+ dx, yh + dy
+        # Compute the movement between new head and previous tail
+        dthx, dthy = xh - xt, yh - yt
+        # If we moved enough, we need to update the tail to the previous head position
+        if abs(dthx) > 1 or abs(dthy) > 1:
+            tail_positions.append(head_positions[-1])
+        head_positions.append((xh, yh))
 
 def plot(positions):
     W = max([x for (x, y) in positions]) + 1
@@ -34,6 +41,8 @@ def plot(positions):
     return s
 
 
-print(plot(head_positions))
-print()
+# print(plot(head_positions))
+# print()
+# print(plot(tail_positions))
+print(len(set(tail_positions)))
 # print(len(set(head_positions[:-1])))
