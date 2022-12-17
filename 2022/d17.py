1fbfa235a6f727473b90807e6e2529d05075e5a2
clemkeirua <clement@keiruaprod.fr>
2022-12-17 14:29:37 +0100

    basic current shape

--- 2022/d17.py
+++ 2022/d17.py
@@ -9,4 +9,20 @@ flat_v = ["#", "#", "#", "#"]
 square = ["##", "##"]
 shapes = [flat_h, cross, reverse_l, flat_v, square]
 
-print(operations)
+class CurrentShape:
+    def __init__(self):
+        self.index = 0
+        self.x = 3
+        self.y = 3+1 # (bottom = 0)
+
+
+# 0 = bottom of the board, so that we can happend new cells easily at the end
+board = [list("#" * (7))] + [list(".......") for l in range(4)]
+def debug(board):
+    for l in board[::-1]:
+        l2 = l.copy()
+        print("".join(l2))
+
+curr_shape = CurrentShape()
+
+debug(board)
