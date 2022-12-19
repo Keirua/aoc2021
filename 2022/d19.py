ef83a8327ccb46f5752bc27c662141dd44d420e5
clemkeirua <clement@keiruaprod.fr>
2022-12-19 15:21:24 +0100

    model edges

--- 2022/d19.py
+++ 2022/d19.py
@@ -13,8 +13,8 @@ def parse(text):
             costs = re.findall("((\d+) (ore|clay|geode|obsidian))+", line)
             w = line.split(" ")
             blueprint = {}
-            blueprint[w[3]] = parse_costs(costs[0][1:])
-            blueprint[w[9]] = parse_costs(costs[1][1:])
+            blueprint[w[3]] = [parse_costs(costs[0][1:])]
+            blueprint[w[9]] = [parse_costs(costs[1][1:])]
             blueprint[w[15]] = [parse_costs(costs[2][1:]), parse_costs(costs[3][1:])]
             blueprint[w[24]] = [parse_costs(costs[4][1:]), parse_costs(costs[5][1:])]
 
@@ -23,13 +23,46 @@ def parse(text):
             # pp.pprint(blueprint)
             out.append(blueprint)
     return out
+from functools import lru_cache
 
-ressources = ["ore", "clay", "obsidian", "geode"]
+def compute_new_ressources(ressources, robots):
+    new_ressources = ressources.copy()
+    for k, v in robots.items():
+        new_ressources[k] += v
+    return new_ressources
 
+
+def best_spend(blueprint, step, ressources, robots):
+    if step == 0:
+        return ressources["geode"]
+    else:
+        possible_scores = [ressources["geode"]]
+        for ressource_name, costs in blueprint.items():
+            # If we have enough ressources to produce said robots, we can build one:
+            if all([ressources[name] > cost for cost, name in costs]):
+                new_ressources = ressources.copy()
+                new_robots = robots.copy()
+                # new_ressources[name] += 1
+                for cost, name in costs:
+                    new_ressources[name] -= cost
+                new_robots[ressource_name] += 1
+                new_ressources = compute_new_ressources(new_ressources, robots)
+                # New edge -> buying a new robot
+                possible_scores.append(best_spend(blueprint, step-1, new_ressources, new_robots))
+        # Then we can also dig for the ressources we own without buying right now
+        new_ressources = compute_new_ressources(ressources, robots)
+        possible_scores.append(best_spend(blueprint, step - 1, new_ressources, robots))
+        return max(possible_scores)
+
+
+ressources_names = ["ore", "clay", "obsidian", "geode"]
 text = open(f"d19.txt").read().strip()
 text_sample = open(f"d19-sample.txt").read().strip()
 # pp.pprint(parse(text))
-pp.pprint(parse(text_sample))
+# pp.pprint()
+blueprints = parse(text_sample)
+from collections import defaultdict
+robots = defaultdict(int)
+robots["ore"] = 1
+print(best_spend(blueprints[0], 24, defaultdict(int), robots))
 
-
-# pp.pprint(blueprints)
