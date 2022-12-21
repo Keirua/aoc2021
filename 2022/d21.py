6fc898e6c7853bc2ba24e5ca77cde19e67b99fba
clemkeirua <clement@keiruaprod.fr>
2022-12-21 14:22:24 +0100

    reimplemented part1 with z3 for part2

--- 2022/d21.py
+++ 2022/d21.py
@@ -4,7 +4,7 @@ OP = {
     "+": lambda a,b: a+b,
     "-": lambda a,b: a-b,
     "*": lambda a,b: a*b,
-    "/": lambda a,b: a//b,
+    "/": lambda a,b: a/b,
 }
 
 lines = open(f"d21.txt").read().splitlines()
@@ -28,8 +28,44 @@ def part1(lines):
             values[k] = v
         return values[k]
 
-    pp.pprint(values)
-    pp.pprint(operations)
+    # pp.pprint(values)
+    # pp.pprint(operations)
     print(get("root"))
 
+
+def part1_z3(lines):
+    values, operations = parse(lines)
+    # values["humn"] = None
+    vars = {}
+    s = Solver()
+    for k in list(operations.keys()) + list(values.keys()):
+        vars[k] = Int(k)
+    for k in values.keys():
+        s.add(vars[k] == values[k])
+    for k in operations.keys():
+        o = operations[k]
+        s.add(vars[k] == OP[o[1]](vars[o[0]], vars[o[2]]))
+    s.check()
+    m = s.model()
+    print(m[vars["root"]].as_long())
+
+from z3 import *
+def part2(lines):
+    values, operations = parse(lines)
+    # values["humn"] = None
+    vars = {}
+    s = Solver()
+    for k in list(operations.keys()) + list(values.keys()):
+        vars[k] = Int(k)
+    for k in values.keys():
+        s.add(vars[k] == values[k])
+    for k in operations.keys():
+        o = operations[k]
+        s.add(vars[k] == OP[o[1]](vars[o[0]], vars[o[2]]))
+    s.check()
+    m = s.model()
+    print(m[vars["root"]].as_long())
+
 part1(lines)
+part2(lines)
+
