55a966ac7333ba387693871dae574e0ea840590a
clemkeirua <clement@keiruaprod.fr>
2022-12-21 14:13:00 +0100

    refactoring

--- 2022/d21.py
+++ 2022/d21.py
@@ -8,21 +8,28 @@ OP = {
 }
 
 lines = open(f"d21.txt").read().splitlines()
-# lines = open(f"d21-sample.txt").read().splitlines()
-values = {}
-operations = {}
-for l in lines:
-    name, content = l.split(": ")
-    try:
-        values[name] = int(content)
-    except:
-        operations[name] = content.split(" ")
+def parse(lines):
+    values = {}
+    operations = {}
+    for l in lines:
+        name, content = l.split(": ")
+        try:
+            values[name] = int(content)
+        except:
+            operations[name] = content.split(" ")
+    return values, operations
 
-def get(k):
-    if k not in values.keys():
-        v = OP[operations[k][1]](get(operations[k][0]), get(operations[k][2]))
-        values[k] = v
-    return values[k]
-pp.pprint(values)
-pp.pprint(operations)
-print(get("root"))
+def part1(lines):
+    values, operations = parse(lines)
+
+    def get(k):
+        if k not in values.keys():
+            v = OP[operations[k][1]](get(operations[k][0]), get(operations[k][2]))
+            values[k] = v
+        return values[k]
+
+    pp.pprint(values)
+    pp.pprint(operations)
+    print(get("root"))
+
+part1(lines)
