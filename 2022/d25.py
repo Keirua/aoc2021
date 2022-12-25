6c1158f50366d81591fd06999b387e12409c9618
clemkeirua <clement@keiruaprod.fr>
2022-12-25 20:39:00 +0100

    part1

--- 2022/d25.py
+++ 2022/d25.py
@@ -1,10 +1,12 @@
 import aoc
 import re, pprint, itertools as it
+
 pp = pprint.PrettyPrinter(indent=4)
 
 input = open(f"d25.txt").read()
-input = open(f"d25-sample.txt").read()
+# input = open(f"d25-sample.txt").read()
 lines = input.split("\n")
+
 
 def sctoi(c):
     try:
@@ -15,14 +17,56 @@ def sctoi(c):
         if c == "=":
             return -2
 
+
 def snafu_to_i(s):
     out = 0
     coef = 1
     for c in s[::-1]:
-        out += sctoi(c)*coef
+        out += sctoi(c) * coef
         coef *= 5
     return out
 
-print(sum(map(snafu_to_i, lines)))
 
-# pp.pprint(lines)
+def i_to_snafu(i: int):
+    out = []
+    coef = 1
+    while coef < i:
+        coef *= 5
+    coef //= 5
+    # First pass where we extract the coefs
+    while coef > 0:
+        val = i // coef
+        i %= coef
+        coef //= 5
+        out.append(val)
+    # Then we remap in the [-2, -1, 0, 1, 2] space with the transforms
+    # described in the examples
+    pos = len(out) -1
+    while(pos) > 0:
+        if out[pos] == 3:
+            out[pos-1] += 1
+            out[pos] = -2
+        if out[pos] == 4:
+            out[pos - 1] += 1
+            out[pos] = -1
+        if out[pos] == 5:
+            out[pos-1] += 1
+            out[pos] = 0
+        pos -= 1
+
+    # Then we convert to str
+    mapping = {
+        -1: "-",
+        -2: "=",
+        0: "0", 1: "1", 2: "2"
+    }
+    return "".join([mapping[o] for o in out])
+
+def part1(lines):
+    s = sum(map(snafu_to_i, lines))
+    print(i_to_snafu(s))
+
+assert (i_to_snafu(10) == "20")
+assert (i_to_snafu(201) == "2=01")
+assert (i_to_snafu(4890) == "2=-1=0")
+part1(lines)
