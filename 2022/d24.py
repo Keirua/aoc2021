c7945904abcc40d02cb5c2573d94475dc0fd3cb4
clemkeirua <clement@keiruaprod.fr>
2022-12-26 15:10:31 +0100

    wip dikjstar

--- 2022/d24.py
+++ 2022/d24.py
@@ -1,5 +1,6 @@
 import pprint as pp, itertools as it
-from collections import Counter
+from collections import Counter, defaultdict
+from heapq import heappop, heappush
 
 DELTAS = {
     ">": (1, 0),
@@ -8,9 +9,12 @@ DELTAS = {
     "^": (0, -1),
 }
 
+
 class Grid:
     def __init__(self, lines):
         self.blizzards = []
+        self.walls = []
+        self.movable = []
         self.W = len(lines[0])
         self.H = len(lines)
         self.start_x = lines[0].index('.')
@@ -21,35 +25,36 @@ class Grid:
             for i, c in enumerate(l):
                 if c in "<>^v":
                     self.blizzards.append((i, j, c))
+                if c == "#":
+                    self.walls.append((i, j))
+                else:
+                    self.movable.append((i, j))
+        self.KNOWN_BLIZZARDS = {}
 
     def next_blizzards(self, blizzards):
         next_blizz = []
         for (x, y, c) in blizzards:
             dx, dy = DELTAS[c]
-            nx, ny = x+dx, y+dy
-            if c == ">" and nx == self.W-1:
+            nx, ny = x + dx, y + dy
+            if c == ">" and nx == self.W - 1:
                 nx = 1
-            if c == "v" and ny == self.H-1:
+            if c == "v" and ny == self.H - 1:
                 ny = 1
             if c == "<" and nx == 0:
-                nx = self.W-2
+                nx = self.W - 2
             if c == "^" and ny == 0:
-                ny = self.H-2
+                ny = self.H - 2
             next_blizz.append((nx, ny, c))
         return next_blizz
 
-    def print_grid(self):
+    def print_grid(self, blizzards):
         out = [["." for _ in range(self.W)] for _ in range(self.H)]
-        for x in range(self.W):
-            out[0][x] = "#"
-            out[-1][x] = "#"
-        for y in range(self.H):
-            out[y][0] = "#"
-            out[y][-1] = "#"
+        for (x, y) in self.walls:
+            out[y][x] = "#"
         out[self.start_y][self.start_x] = 'S'
         out[self.end_y][self.end_x] = 'E'
-        counter = Counter([(x, y) for (x, y, c) in self.blizzards])
-        for (x, y, c) in self.blizzards:
+        counter = Counter([(x, y) for (x, y, c) in blizzards])
+        for (x, y, c) in blizzards:
             if counter[(x, y)] == 1:
                 out[y][x] = c
         for (x, y), v in counter.items():
@@ -58,15 +63,88 @@ class Grid:
 
         return "\n".join(["".join(l) for l in out])
 
+    def dbg(self, blizzards, x, y):
+        out = [["." for _ in range(self.W)] for _ in range(self.H)]
+        for (x, y) in self.walls:
+            out[y][x] = "#"
+        counter = Counter([(x, y) for (x, y, c) in blizzards])
+        for (x, y, c) in blizzards:
+            if counter[(x, y)] == 1:
+                out[y][x] = c
+        for (x, y), v in counter.items():
+            if v > 1:
+                out[y][x] = str(v)
+        out[y][x] = "E"
+
+        return "\n".join(["".join(l) for l in out])
+
+    # def all_movable_coords(self):
+    #     "useless"
+    #     movable = []
+    #     for x in range(self.W):
+    #         for y in range(self.H):
+    #             if (x,y) not in self.walls:
+    #                 movable.append((x,y))
+    #     return movable
+
+    def get_blizzard_at(self, t):
+        if t in self.KNOWN_BLIZZARDS:
+            return self.KNOWN_BLIZZARDS[t]
+        blizz = self.blizzards
+        while t > 0:
+            blizz = self.next_blizzards(blizz)
+            t -= 1
+        self.KNOWN_BLIZZARDS[t] = blizz
+        return blizz
+
+    def solve(self):
+        """
+        Dijkstra in moving map
+        """
+        Q = [(0, 0, (self.start_x, self.start_y))]
+        DIST = {(x, y, 0): 1000000000000000 for (x, y) in self.movable}
+        PREV = {(x, y, 0): None for (x, y) in self.movable}
+        while len(Q) > 0:
+            min_dist, t, (x, y) = heappop(Q)
+            if (x, y) == (self.end_x, self.end_y):
+                print(t, x, y)
+                return t, PREV
+            # We have a better path already
+            if (x, y, t) in DIST and DIST[(x, y, t)] < min_dist:
+                continue
+            # Generate the possible next moves
+            next_blizzard = self.get_blizzard_at(t + 1)
+            for (dx, dy) in [(-1, 0), (1, 0), (0, -1), (0, 1), (0, 0)]:
+                nx, ny = x + dx, y + dy
+                new_dist = DIST[(x, y, t)] + 1
+                if 0 <= nx <= self.W-1 and 0<= ny <= self.H-1 and (nx, ny) not in self.walls and (nx, ny) not in next_blizzard:
+                    if (nx, ny, t + 1) not in DIST or DIST[(nx, ny, t + 1)] > new_dist:
+                        DIST[(nx, ny, t + 1)] = new_dist
+                        PREV[(nx, ny, t + 1)] = (x, y)
+                        heappush(Q, (new_dist, t + 1, (nx, ny)))
+
 
 lines = open(f"d24.txt").read().strip().splitlines()
 lines = open(f"d24-sample.txt").read().strip().splitlines()
 lines = open(f"d24-sample2.txt").read().strip().splitlines()
 g = Grid(lines)
 pp.pprint(g.blizzards)
+t, prev = g.solve()
+history = []
 for i in range(10):
-    print(g.print_grid())
-    g.blizzards = g.next_blizzards(g.blizzards)
+    idx = (6, 5, i)
+    if idx in prev.keys():
+        print(idx, prev[idx])
+# x, y = prev[(g.end_y, g.end_x, t+1)]
+# while (x, y) != (g.start_x, g.start_y):
+#     history.append((x, y))
+#     (x, y) = prev[(x, y, t-1)]
+#     t -= 1
+
+# print(history)
+# for i in range(10):
+#     print(g.print_grid(g.blizzards))
+#     g.blizzards = g.next_blizzards(g.blizzards)
     # print(g.blizzards)
-    print()
+#     print()
 # pp.pprint(lines)
