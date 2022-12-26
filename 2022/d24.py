a43d81117ce7b4519a38335b7c71c3b1dde311b3
clemkeirua <clement@keiruaprod.fr>
2022-12-26 14:35:09 +0100

    parsing, blizzard and debug code

--- 2022/d24.py
+++ 2022/d24.py
@@ -1,10 +1,5 @@
-import aoc
-import re, pprint, itertools as it
-
-pp = pprint.PrettyPrinter(indent=4)
-
-lines = open(f"d24.txt").read().strip().splitlines()
-lines = open(f"d24-sample.txt").read().strip().splitlines()
+import pprint as pp, itertools as it
+from collections import Counter
 
 DELTAS = {
     ">": (1, 0),
@@ -32,6 +27,16 @@ class Grid:
         for (x, y, c) in blizzards:
             dx, dy = DELTAS[c]
             nx, ny = x+dx, y+dy
+            if c == ">" and nx == self.W-1:
+                nx = 1
+            if c == "v" and ny == self.H-1:
+                ny = 1
+            if c == "<" and nx == 0:
+                nx = self.W-2
+            if c == "^" and ny == 0:
+                ny = self.H-2
+            next_blizz.append((nx, ny, c))
+        return next_blizz
 
     def print_grid(self):
         out = [["." for _ in range(self.W)] for _ in range(self.H)]
@@ -43,12 +48,25 @@ class Grid:
             out[y][-1] = "#"
         out[self.start_y][self.start_x] = 'S'
         out[self.end_y][self.end_x] = 'E'
+        counter = Counter([(x, y) for (x, y, c) in self.blizzards])
+        for (x, y, c) in self.blizzards:
+            if counter[(x, y)] == 1:
+                out[y][x] = c
+        for (x, y), v in counter.items():
+            if v > 1:
+                out[y][x] = str(v)
 
-        return "\n".join(["".join(l) for l in lines])
+        return "\n".join(["".join(l) for l in out])
 
 
-
+lines = open(f"d24.txt").read().strip().splitlines()
+lines = open(f"d24-sample.txt").read().strip().splitlines()
+lines = open(f"d24-sample2.txt").read().strip().splitlines()
 g = Grid(lines)
 pp.pprint(g.blizzards)
-print(g.print_grid())
+for i in range(10):
+    print(g.print_grid())
+    g.blizzards = g.next_blizzards(g.blizzards)
+    # print(g.blizzards)
+    print()
 # pp.pprint(lines)
